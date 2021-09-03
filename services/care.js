eb44309d8416ee406e154126b3e7ea4543c2f3f2
Arzel <arzelcanavate@gmail.com>
2021-09-03 12:47:06 +0200

    add last test

--- services/care.js
+++ services/care.js
@@ -127,7 +127,7 @@ module.exports = class Care {
         }])
         break;
       case "care_help":
-        let response = await fetch(new URL('https://www.depisoenpiso.com/old_root/php/controllers/test.php'), {
+        response = await fetch(new URL('https://www.depisoenpiso.com/old_root/php/controllers/test.php'), {
           method: "GET"
         });
 
